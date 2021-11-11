cd2ce0f842996eaf999225d86204dca7e8f1c220
Justin Dowell <JulesLetters@gmail.com>
2021-11-11 08:19:03 -0600

    Make frame indexing work, correct 'layer' to 'frame'

--- choose_your_own_animation/choose_your_own_animation.py
+++ choose_your_own_animation/choose_your_own_animation.py
@@ -1,22 +1,27 @@
 import datetime
 import re
 from collections import defaultdict
-from typing import List, Dict, Set
+from typing import List, Dict, Set, Tuple
 
 from PyQt5.QtCore import QSize, Qt
 from PyQt5.QtGui import QStandardItem, QStandardItemModel, QIcon, QPixmap
-from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QListView, QPushButton, QWidget, QSplitter, QSpinBox, QTextEdit
+from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QListView, QPushButton, QWidget, QSplitter, QSpinBox, QTextEdit, \
+    QLineEdit
 from krita import Krita, Node, DockWidget, DockWidgetFactory, DockWidgetFactoryBase
 
 
 class ChooseYourOwnAnimation(DockWidget):
     WINDOW_TITLE = "Choose Your Own Animation"
-    ANIMATION_LAYER_NAME = "Animation"
+    ANIMATION_ROOT_LAYER_NAME = "Animation"
+    FRAMES_ROOT_LAYER_NAME = 'Frames'
+    COMMENT_PATTERN = re.compile(r"\[.*?]")
+    LAYER_NAME_PATTERN = re.compile(r"(?P<name>\S+)\s*(?:\((?P<aliases>[^()]+)\))? - (?P<dests>.+)")
 
     def __init__(self):
         super().__init__()
         self.full_log = ""
-        self.layer_name_to_destination_nodes: Dict[str, Set[Node]] = {}
+        self.frame_name_to_node: Dict[str, Node] = {}
+        self.frame_name_to_destination_nodes: Dict[str, List[Node]] = {}
 
         self.setWindowTitle(self.WINDOW_TITLE)
         base_widget = QWidget()
@@ -50,7 +55,11 @@ class ChooseYourOwnAnimation(DockWidget):
         frames_to_add_spinner.setValue(2)
         left_layout.addWidget(frames_to_add_spinner)
 
-        self.button_refresh = QPushButton("Refresh Frames")
+        self.current_frame_name_widget = QLineEdit()
+        self.current_frame_name_widget.editingFinished.connect(self.refresh_choices_if_modified)
+        left_layout.addWidget(self.current_frame_name_widget)
+
+        self.button_refresh = QPushButton("Refresh Frame Index")
         self.button_refresh.clicked.connect(self.refresh)
         left_layout.addWidget(self.button_refresh)
 
@@ -78,7 +87,7 @@ class ChooseYourOwnAnimation(DockWidget):
         self.future_frames_list.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.future_frames_list.setIconSize(QSize(128, 128))
         self.future_frames_list.setResizeMode(QListView.Adjust)
-        # self.future_frames_list.doubleClicked.connect()
+        self.future_frames_list.doubleClicked.connect(self.choice_double_clicked)
 
         self.frames_model = QStandardItemModel()
         self.future_frames_list.setModel(self.frames_model)
@@ -101,10 +110,15 @@ class ChooseYourOwnAnimation(DockWidget):
         time = datetime.datetime.now()
         self.full_log += f"{time} - {prefix} - {text}\n"
         self.log_text_area.setPlainText(self.full_log)
+        self.scroll_log_to_bottom()
 
     def clear_log(self) -> None:
         self.full_log = ""
         self.log_text_area.setPlainText("")
+
+    def scroll_log_to_bottom(self) -> None:
+        vertical_scroll_bar = self.log_text_area.verticalScrollBar()
+        vertical_scroll_bar.setValue(vertical_scroll_bar.maximum())
 
     def do_a_thing(self):
         self.log_info("Doing the thing!")
@@ -128,54 +142,62 @@ class ChooseYourOwnAnimation(DockWidget):
             self.frames_model.appendRow(temp_item)
         self.log_info("Exiting Doing the thing function.")
 
-    def refresh(self):
+    def refresh(self) -> None:
+        self.refresh_frame_index()
+        self.refresh_choices()
+
+    def refresh_frame_index(self) -> None:
         self.log_info(f"Refreshing future frame index...")
+        frame_name_to_node, frame_name_to_aliases, frame_name_to_destinations = self.get_layer_information()
+
+        self.frame_name_to_node = frame_name_to_node
+
+        # What Node is being talked about when an alias is given. Since anything can claim an alias, we get a Set.
+        destination_name_to_frame_name: Dict[str, Set[str]] = defaultdict(set)
+        for frame_name, aliases in frame_name_to_aliases.items():
+            for alias in aliases:
+                destination_name_to_frame_name[alias].add(frame_name)
+
+        # Finally, assemble the data structure we can query for what frames come next.
+        self.frame_name_to_destination_nodes.clear()
+        for frame_name, aliases in frame_name_to_destinations.items():
+            destination_frame_names: Set[str] = set()
+            for alias in aliases:
+                if alias not in destination_name_to_frame_name:
+                    self.log_warning(f"Layer not found for alias: {alias}")
+                    continue
+                destination_frame_names.update(destination_name_to_frame_name[alias])
+
+            unique_destination_nodes = [self.frame_name_to_node[alias] for alias in destination_frame_names]
+            self.frame_name_to_destination_nodes[frame_name] = unique_destination_nodes
+
+        self.log_info(f"Done refreshing future frame index.")
+
+    def get_layer_information(self) -> Tuple[Dict[str, Node], Dict[str, List[str]], Dict[str, List[str]]]:
         leaf_nodes = self.get_leaf_nodes()
 
-        comment_pattern = re.compile(r"\[.*?]")
-        layer_name_pattern = re.compile(r"(?P<name>\S+)\s*(?:\((?P<aliases>[^()]+)\))? - (?P<dests>.+)")
-
-        layer_name_to_node: Dict[str, Node] = {}
-        layer_name_to_aliases: Dict[str, List[str]] = {}
-        layer_name_to_destinations: Dict[str, List[str]] = {}
+        frame_name_to_node: Dict[str, Node] = {}
+        frame_name_to_aliases: Dict[str, List[str]] = {}
+        frame_name_to_destinations: Dict[str, List[str]] = {}
         for leaf in leaf_nodes:
-            name_with_removed_comments = comment_pattern.sub("", leaf.name())
-            match = layer_name_pattern.fullmatch(name_with_removed_comments)
+            name_with_removed_comments = self.COMMENT_PATTERN.sub("", leaf.name())
+            match = self.LAYER_NAME_PATTERN.fullmatch(name_with_removed_comments)
             if not match:
                 self.log_warning(f"Skipping incorrectly named layer: {leaf.name()}")
                 continue
 
-            layer_name = match.group('name')
-            layer_aliases = match.group('aliases')
-            layer_destinations = match.group('dests')
-            if layer_name in layer_name_to_node:
-                self.log_warning(f"Skipping layer with duplicate name: {layer_name}")
+            frame_name = match.group('name')
+            frame_aliases = match.group('aliases')
+            frame_destinations = match.group('dests')
+            if frame_name in frame_name_to_node:
+                self.log_warning(f"Skipping layer with duplicate name: {frame_name}")
                 continue
-            layer_name_to_node[layer_name] = leaf
-            layer_name_to_aliases[layer_name] = layer_aliases.split(" ") if layer_aliases else []
-            layer_name_to_aliases[layer_name].extend([layer_name])  # A layer name is an alias to itself.
-            layer_name_to_destinations[layer_name] = layer_destinations.split(" ") if layer_destinations else []
+            frame_name_to_node[frame_name] = leaf
+            frame_name_to_aliases[frame_name] = frame_aliases.split(" ") if frame_aliases else []
+            frame_name_to_aliases[frame_name].extend([frame_name])  # A layer name is an alias to itself.
+            frame_name_to_destinations[frame_name] = frame_destinations.split(" ") if frame_destinations else []
 
-        # What Node is being talked about when an alias is given. Since anything can claim an alias, we get a Set.
-        destination_name_to_nodes: Dict[str, Set[Node]] = defaultdict(set)
-        for layer_name, aliases in layer_name_to_aliases.items():
-            destination_node = layer_name_to_node[layer_name]
-            for alias in aliases:
-                destination_name_to_nodes[alias] += destination_node
-
-        # Finally, assemble the data structure we can query for what frames come next.
-        self.layer_name_to_destination_nodes.clear()
-        for layer_name, aliases in layer_name_to_destinations.items():
-            destination_nodes = set()
-            for alias in aliases:
-                if alias not in destination_name_to_nodes:
-                    self.log_warning(f"Layer not found for alias: {alias}")
-                    continue
-                destination_nodes.update(destination_name_to_nodes[alias])
-            self.layer_name_to_destination_nodes[layer_name] = destination_nodes
-
-        self.log_info(f"Done refreshing future frame index.")
-        # sorted(layers, key=attrgetter('name'))   # <--How to sort the Nodes by name later, for display.
+        return frame_name_to_node, frame_name_to_aliases, frame_name_to_destinations
 
     def get_leaf_nodes(self) -> List[Node]:
         doc = self.get_active_document()
@@ -183,9 +205,9 @@ class ChooseYourOwnAnimation(DockWidget):
             self.log_error("Make or open a document.")
             return []
 
-        frames_group = doc.nodeByName('Frames')
+        frames_group = doc.nodeByName(self.FRAMES_ROOT_LAYER_NAME)
         if not frames_group:
-            self.log_error("Make a Group named 'Frames'.")
+            self.log_error(f"Make a Group named '{self.FRAMES_ROOT_LAYER_NAME}'.")
             return []
 
         return self.recursively_get_leaf_nodes(frames_group)
@@ -198,6 +220,31 @@ class ChooseYourOwnAnimation(DockWidget):
         else:
             result.append(root_node)
         return result
+
+    def refresh_choices_if_modified(self) -> None:
+        if self.current_frame_name_widget.isModified():
+            self.current_frame_name_widget.setModified(False)
+            self.refresh_choices()
+
+    def refresh_choices(self) -> None:
+        self.log_info("Refreshing future frame choices...")
+
+        frame_name = self.current_frame_name_widget.text()
+        if frame_name not in self.frame_name_to_destination_nodes:
+            self.log_error(f"No frames in index for: {frame_name}")
+            return
+
+        self.frames_model.clear()
+        # sorted(, key=attrgetter('name'))   # <--How to sort the Nodes by name later, for display.
+        destination_nodes = self.frame_name_to_destination_nodes[frame_name]
+        for dn in destination_nodes:
+            frame_icon = QIcon(QPixmap.fromImage(dn.thumbnail(128, 128)))
+            self.frames_model.appendRow(QStandardItem(frame_icon, dn.name()))
+
+        self.log_info("Finished refreshing future frame choices.")
+
+    def choice_double_clicked(self, clicked_index) -> None:
+        self.log_info(f"User picked index {clicked_index}")
 
     @staticmethod
     def get_active_document():
@@ -226,7 +273,7 @@ class ChooseYourOwnAnimation(DockWidget):
         return layer
 
     def get_or_create_animation_layer(self):
-        animation_layer = self.get_or_create_layer(self.ANIMATION_LAYER_NAME)
+        animation_layer = self.get_or_create_layer(self.ANIMATION_ROOT_LAYER_NAME)
         if not animation_layer.animated():
             animation_layer.enableAnimation()
         return animation_layer
